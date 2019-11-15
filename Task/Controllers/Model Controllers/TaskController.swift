521bdf497a711943bc324c6d14c95758d60d6005
Aaron Shackelford <burntcreamlatte@icloud.com>
2019-11-14 17:21:00 -0700

    first commit

---
+++ Task/Controllers/Model Controllers/TaskController.swift
@@ -0,0 +1,9 @@
+//
+//  TaskController.swift
+//  Task
+//
+//  Created by Aaron Shackelford on 11/13/19.
+//  Copyright © 2019 Aaron Shackelford. All rights reserved.
+//
+
+import Foundation
---
+++ Task/Resources/DateHelpers.swift
@@ -0,0 +1,9 @@
+//
+//  DateHelpers.swift
+//  Task
+//
+//  Created by Aaron Shackelford on 11/13/19.
+//  Copyright © 2019 Aaron Shackelford. All rights reserved.
+//
+
+import Foundation
